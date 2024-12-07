664bac32049af2f782167b469db3966a6945b410
ZoitovNurillo <nurillozoitov97@gmail.com>
2024-12-07 10:08:30 +0500

    Initial commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,16 +5,16 @@ import AppFilter from '../app-filter/app-filter';
 import SearchPanel from '../search-panel/search-panel';
 import MovieList from '../movie-list/movie-list';
 import MoviesAddForm from '../movies-add-form/movies-add-form';
-
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       data: [
-        {name:'Empire of Osmon', viewers: 600, favourite:false, id:1},
-        {name:'Ertugrul', viewers: 222, favourite:false, id:2},
-        {name:'Alpomish', viewers: 442, favourite:true, id:3},
+        {name:'Empire of Osmon', viewers: 600, favourite:false, like:false, id:1},
+        {name:'Ertugrul', viewers: 222, favourite:false, like:false, id:2},
+        {name:'Alpomish', viewers: 442, favourite:false, like:false, id:3},
       ],
     }
   }
@@ -24,6 +24,39 @@ class App extends Component {
         data: data.filter(item => item.id !== id)
     }))
   }
+
+  addForm = ( item) => {
+    const newItem = {name: item.name, viewers: item.viewers, id: uuidv4(), favourite: false, like:false}
+    this.setState(({data}) => ({
+        data: [...data, newItem],
+    }))
+  }
+
+  onToggleProp = (id, prop) => {
+    this.setState(({data}) => {
+      return {
+        data: data.map(item => {
+          if (item.id ===id) {
+            return {...item, [prop]: !item[prop]}
+          }
+          return item;
+        }),
+      }
+    })
+  }
+
+  // onToggleLike = (id) => {
+  //   this.setState(({data}) => {
+  //     return {
+  //       data: data.map(item => {
+  //         if (item.id ===id) {
+  //           return {...item, like: !item.like}
+  //         }
+  //         return item;
+  //       }),
+  //     }
+  //   })
+  // }
 
   render() {
     const {data} = this.state;
@@ -35,8 +68,8 @@ class App extends Component {
             <SearchPanel/>
             <AppFilter/>
           </div>
-          <MovieList data={data} onDelete={this.onDelete}/>
-          <MoviesAddForm/>.
+          <MovieList onToggleProp={this.onToggleProp} data={data} onDelete={this.onDelete}/>
+          <MoviesAddForm addForm={this.addForm}/>.
         </div>
       </div>
     )
--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -5,8 +5,8 @@ class MoviesAddForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '+99890',
-      views: '555',
+      name: '',
+      views: '',
     }
   }
 
@@ -17,12 +17,18 @@ class MoviesAddForm extends Component {
     })
   }
 
+  addFormHandler = (e) => {
+    e.preventDefault()
+    this.props.addForm({name: this.state.name, viewers: this.state.views})
+    this.setState({name:'', views:''})
+  }
+
   render() {
-    const {name, views} = this.state
+    const {name, views} = this.state;
     return (
       <div className='movies-add-form'>
         <h3>Yangi kino qo'shish</h3>
-        <form className="add-form d-flex">
+        <form className="add-form d-flex" onSubmit={this.addFormHandler}>
           <input onChange={this.changeHandlerInput} type="text" className='form-control new-post-label' placeholder='Qanday kino' name='name' value={name} />
           <input onChange={this.changeHandlerInput} type="number" className='form-control new-post-label' placeholder="Nechi marta ko'rilgan" name='views' value={views} />
           <button type='submit' className='btn btn-outline-dark'>
